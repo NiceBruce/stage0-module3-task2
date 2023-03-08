a3a98287982c81c9899dc1b8eb79e354af9b16ea
Boris Demin <demin.b.d@gmail.com>
2023-03-08 15:43:49 +0300

    "Module3Task2 is Done!"

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -3,12 +3,16 @@ package lang.print.gaps.task2;
 public class CopiedNames {
     public static void main(String[] args) {
         int variable = 0;
-        int variable = 2000;
-        int value = 1000;
-        int variable = 4000;
-        int value2 = 3000;
+        int nextVariable = 2000;
+        int firstValue = 1000;
+        int thirdVariable = 4000;
+        int secondvalue = 3000;
         int variables = 5000;
         variables = 7000;
         int value = 90000;
+        int intVal = 100000;
+        int myFirstInteger = 1000000;
+        int my_var = 1234567;
+        int mySecondInteger = 1234567;
     }
 }
